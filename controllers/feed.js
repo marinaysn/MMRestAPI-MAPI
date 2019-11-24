f1cdb617cdd1c3141f9683d5905ae6637b4e7e43
MarinaYsn <marinaysn@gmail.com>
2019-11-23 20:15:55 -0500

    add create post endpoints an d images

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,19 +1,23 @@
 
 exports.getPosts = ((req, res, next)=>{
 
-    res.status(200).json({posts: [{title: 'Creating REST APIs with Node.js & TypeScript', content: 'A WebAPI consisting of endpoints to a request–response message system (JSON/XML) exposed as an HTTP-based server', url: 'https://www.reddit.com/r/learnjavascript/comments/dzqbay/creating_rest_apis_with_nodejs_typescript_part_1/'}]});
+    res.status(200).json({posts: [{
+        _id: '1',
+        title: 'Creating REST APIs with Node.js & TypeScript', content: 'A WebAPI consisting of endpoints to a request–response message system (JSON/XML) exposed as an HTTP-based server', createdAt: new Date(), imageUrl: 'images/1.jpg', creator: {
+        name: 'Marina'
+    }}]});
 });
 
 exports.createPost = ( req, res, next) => {
     //Create post in db
     const title = req.body.title;
     const content = req.body.content;
-    const url = req.body.url;
-
+    const imageUrl = req.body.imageUrl;
+    const date = new Date();
 
     res.status(201).json({
         message: 'Post Created!',
-        post: {id: new Date().toISOString(), title: title, content: content, url: url}
+        post: {_id: new Date().toISOString(), title: title, content: content, imageUrl: 'images/1.jpg', createdAt: date, creator: {name: 'Anna'}}
     })
 };
 
