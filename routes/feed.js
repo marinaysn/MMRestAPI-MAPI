e670d9354c860e8c740f80d912c4bd1e3a962944
MarinaYsn <marinaysn@gmail.com>
2019-11-28 09:28:13 -0500

    added routines for update and delete item

--- routes/feed.js
+++ routes/feed.js
@@ -24,4 +24,22 @@ feedController.createPost);
 
 router.get('/post/:postId', feedController.getSinglePost);
 
+
+router.put('/post/:postId',
+[
+    body('title', 'Please enter valid title!')
+    .trim()
+    .isLength({min: 5})
+    .isString(),
+
+    body('content', 'Please enter valid content')
+    .trim()
+    .isLength({min: 5, max: 5500 })
+    .isString()
+
+], 
+feedController.updatePost
+)
+
+router.delete('/post/:postId', feedController.deletePost);
 module.exports = router;
