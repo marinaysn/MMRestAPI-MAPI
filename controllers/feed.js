f40a41f1e86e0389b6f0ca4266a46afef3664081
MarinaYsn <marinaysn@gmail.com>
2019-12-01 15:51:13 -0500

    added update and delete routines with socket.io

--- controllers/feed.js
+++ controllers/feed.js
@@ -19,6 +19,7 @@ exports.getPosts = async (req, res, next) => {
 
         const posts = await Post.find()
             .populate('creator')
+            .sort({createdAt: -1 })
             .skip((currentPage - 1) * ITEMS_PER_PAGE)
             .limit(ITEMS_PER_PAGE);
 
@@ -80,7 +81,7 @@ exports.createPost = async (req, res, next) => {
 
         // sending notification about the change to ALL clients
         // to send responce to all clients except the one who send request - use broadcast instead
-        io.getIO().emit('postsListened', {action: 'create', post: {...post._doc, creator: { _id: req.userId, name: user.name}}});
+        io.getIO().emit('postsListener', {action: 'create', post: {...post._doc, creator: { _id: req.userId, name: user.name}}});
 
 
         res.status(201).json({
@@ -155,14 +156,9 @@ exports.updatePost = async (req, res, next) => {
             err.httpStatusCode = 404;
             throw err;
         }
- console.log('rrrrrrrrrrr')
-            console.log(post.creator._id.toString() 
-            )
-            console.log(req.userId)
-       
+
         //check if user is authorized to update post
-        if (post.creator._id.toString() !== req.userId) {
-           
+        if (post.creator._id.toString() !== req.userId) {     
             const error = new Error('Not Authorized!');
             error.statusCode = 403;
             throw error;
@@ -176,13 +172,8 @@ exports.updatePost = async (req, res, next) => {
         post.imageUrl = imageUrl;
 
         const result = await post.save();
-        console.log('11111111111111')
-        //console.log(post)
-        console.log('-----------------')
-       // console.log(result)
-      //  io.getIO.emit('postsListened', {action: 'update', post: result})
-      io.getIO().emit('posts', { action: 'update', post: result });
-      console.log('2222222222222')
+        io.getIO().emit('postsListener', { action: 'update', post: result });
+
         res.status(200).json({ message: 'Post updated', post: result });
     }
     catch (err) {
@@ -222,6 +213,11 @@ exports.deletePost = async (req, res, next) => {
 
         user.posts.pull(postId);
         await user.save()
+
+         // sending notification about the deleted posts to ALL clients
+        io.getIO().emit('postsListener', {action: 'delete', post: postId});
+
+
         res.status(200).json({ message: 'Deleted Post' });
 
     } catch (err) {
